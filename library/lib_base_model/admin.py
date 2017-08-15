f51801acfe7e1f6914ab500c04c8b18d4226f15f
Sajjad Heydari <mcshemail@gmail.com>
2017-08-15 15:07:55 +0430

    admin

--- library/lib_base_model/admin.py
+++ library/lib_base_model/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Book, Publisher, Section, User as My_User, Membership, Borrow
+
+from django.contrib.auth.models import User, Group
+
 # Register your models here.
+
+
+admin.site.register(Book)
+admin.site.register(Publisher)
+admin.site.register(Section)
+admin.site.register(My_User)
+admin.site.register(Membership)
+admin.site.register(Borrow)
+
--- library/lib_base_model/models.py
+++ library/lib_base_model/models.py
@@ -23,6 +23,8 @@ class Book(models.Model):
 class Section(models.Model):
     name = models.CharField(max_length=32)
     
+    def __str__(self):
+        return "Section " + self.name
 
 class User(models.Model):
     django_user = models.OneToOneField(DjangoUser)
@@ -32,6 +34,8 @@ class User(models.Model):
         )
     role = models.IntegerField(choices=role_choice)
     
+    def __str__(self):
+        return str(self.django_user)
 
 class Membership(models.Model):
     book = models.OneToOneField(Book)
@@ -41,9 +45,23 @@ class Membership(models.Model):
     
     def adrs(self):
         return self.shelf + ', ' + self.row
+
+    def __str__(self):
+        return str(self.book) + " is located at " + str(self.section) + " Shelf " + str(self.shelf) + " Row: " + str(self.row)
     
 class Borrow(models.Model):
-    start = models.DateField()
-    end = models.DateField()
+    start = models.DateField(null=True)
+    end = models.DateField(null=True)
     user = models.ForeignKey(User)
     book = models.ForeignKey(Book)
+
+    def __str__(self):
+        return str(self.book) + " to " + str(self.user) + " at " + str(self.start) + " until " + str(self.end)
+
+    def state(self):
+        if start == None:
+            return "Reserved"
+        elif end == None:
+            return "Taken"
+        else:
+            return "Brought Back"
