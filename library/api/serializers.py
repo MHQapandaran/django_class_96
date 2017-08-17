31752876709ce3eee5929fd658e81a88c7c7ae93
Sajjad Heydari <mcshemail@gmail.com>
2017-08-17 15:13:51 +0430

    Added api

---
+++ library/api/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+from lib_base_model.models import Book, Publisher
+
+#class PublisherSerializer(serializers.Serializer):
+    #id = serializers.IntegerField(read_only=True)
+    #name = serializers.CharField(max_length=32, required=True)
+    
+    #def create(self, validated_data):
+        #return Publisher.objects.create(**validated_data)
+    
+    #def update(self, instance, validated_data):
+        #instance.name = validated_data.get("name")
+        #instance.save()
+        #return instance
+    
+
+#class BookSerializer(serializers.Serializer):
+    #id = serializers.IntegerField(read_only=True)
+    #name = serializers.CharField(max_length=64, required=True)
+    #ISBN = serializers.CharField(max_length=20, required=True)
+    #summary = serializers.TextField(required=False)
+    #publisher = serializers.PublisherSerializer()
+    
+    #def create(self, validated_data):
+        #b = Book()
+        #b.name = validated_data.get("name")
+        #b.ISBN = validated_data.get("ISBN")
+        #b.summary = validated_data.get("summary")
+        #b.publisher = validated_data.get("publisher")
+        #b.save()
+        #return b
+    
+    #def update(self, instance, validated_data):
+        
+        #pass
+
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = ('id', 'name')
+
+class BookSerializer(serializers.ModelSerializer):
+    
+    publisher = PublisherSerializer()
+    
+    class Meta:
+        model = Book
+        fields = ('id', 'name', 'ISBN', 'summary', 'publisher')
+
